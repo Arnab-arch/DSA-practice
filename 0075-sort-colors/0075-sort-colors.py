8249ded065792332ea855ea108a640e72ba731ef
Arnab Sheoran <arnabsheoran801@gmail.com>
2026-06-26 14:29:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -13,32 +13,50 @@ class Solution(object):
         #return nums
 
         #optimal approach 
-        n=len(nums)
-        zero = 0 
-        one = 0 
-        two = 0
+        #n=len(nums)
+        #zero = 0 
+        #one = 0 
+        #two = 0
 
-        for num in nums:
-            if num == 0 :
-                zero+=1
-            elif num ==1:
-                one+=1
-            else:
-                two+=1
-        i=0
-        while zero:
-            nums[i]=0
-            i+=1
-            zero-=1
-        while one:
-            nums[i]=1
-            i+=1
-            one-=1
-        while two:
-            nums[i]=2
-            i+=1
-            two-=1
+        #for num in nums:
+        #    if num == 0 :
+        #        zero+=1
+         #   elif num ==1:
+         #       one+=1
+         #   else:
+         #       two+=1
+        #i=0
+        #while zero:
+        #    nums[i]=0
+        #    i+=1
+        #    zero-=1
+        #while one:
+        #    nums[i]=1
+        #    i+=1
+        #    one-=1
+        #while two:
+        #    nums[i]=2
+        #    i+=1
+        #    two-=1
+        #return nums
+
+      #dutch flag algorithm 
+        n=len(nums)
+        l=0
+        m=0
+        h=n-1
+        while m<=h:
+            if nums[m]== 0:
+                nums[l],nums[m]=nums[m],nums[l]
+                l+=1
+                m+=1
+            elif nums[m]==1:
+                m+=1
+            elif nums[m]==2:
+                nums[m],nums[h]=nums[h],nums[m]
+                h-=1
         return nums
+
 
 
 
